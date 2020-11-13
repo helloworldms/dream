a5fde27194890ed2fe4f9a75415a060a6bcff817
helloworldms <72796025+helloworldms@users.noreply.github.com>
2020-11-13 23:23:03 +0900

    todolist

--- Chapter1/coordinates/main.js
+++ Chapter1/coordinates/main.js
@@ -3,18 +3,22 @@ const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  console.log(`${x} ${y}`);
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.cssText = "color: yellow; border: 1px solid black";
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.innerHTML = `${x}px, ${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px )`;
+    tag.style.transform = `translate(${x}px, ${y}px )`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
 });
---
+++ Chapter1/script.js
@@ -0,0 +1,4 @@
+var li = document.createElement("li");
+var textnode = document.createTextNode("Water");
+li.appendChild(textnode);
+document.getElementById("myList").appendChild(li);
