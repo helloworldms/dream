f195364906ed33c370dce59b3b463e6eb4cad41b
helloworldms <72796025+helloworldms@users.noreply.github.com>
2020-11-15 23:44:20 +0900

    classlist

--- Chapter1/carrot/main.js
+++ Chapter1/carrot/main.js
@@ -1,10 +1,76 @@
+const CARROT_SIZE = 120;
+const CARROT_COUNT = 10;
+const BUG_COUNT = 10;
 
-
-const field = document.querySelector('.game__filed');
+const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
 
-function initGame(){
+let started = false;
+let score = 0;
+let timer = undefined;
 
+gameBtn.addEventListener("click", () => {
+  console.log("he");
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
 
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
 
+function stopGamer() {}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = " visible";
+  gameScore.style.visibility = " visible";
+}
+function showStopButton() {
+  const icon = gameBtn.querySelector(".play");
+  icon.classList.add("stop");
+  icon.setAttribute("src", "img/stop.png");
+}
+
+function stopGame() {}
+
+console.log(fieldRect);
+
+function initGame() {
+  gameScore.innerText = CARROT_COUNT;
+  field.innerHTML = "";
+  addItem("carrot", CARROT_COUNT, "img/carrot.png");
+  addItem("bug", BUG_COUNT, "img/bug.png");
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement("img");
+    item.setAttribute("class", className);
+    item.setAttribute("src", imgPath);
+    item.style.position = "absolute";
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
 }
--- Chapter1/script.js
+++
@@ -1,4 +0,0 @@
-var li = document.createElement("li");
-var textnode = document.createTextNode("Water");
-li.appendChild(textnode);
-document.getElementById("myList").appendChild(li);
