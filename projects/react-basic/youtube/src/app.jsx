0b98c9e885c65890f94417108028084f92de10fc
helloworldms <72796025+helloworldms@users.noreply.github.com>
2020-12-15 23:07:59 +0900

    react hook

--- projects/react-basic/youtube/src/app.jsx
+++ projects/react-basic/youtube/src/app.jsx
@@ -1,13 +1,25 @@
-import "./app.css";
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  const name = "ellie";
+  const [videos, setVideos] = useState([]);
 
-  return (
-    <>
-      <h1>Hel {name}</h1>;
-    </>
-  );
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAtrSZkopq--QXlpEYQ5SrM9Kg5TZlZMl0',
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
